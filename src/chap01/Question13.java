cf755d8e1164384868f379007477c468a9726398
ektkfk180 <81549627+ektkfk180@users.noreply.github.com>
2021-11-21 20:15:58 +0900

    덧셈을 출력하는 프로그램

---
+++ src/chap01/Question13.java
@@ -0,0 +1,26 @@
+package chap01;
+
+public class Question13 {
+
+    public static void main(String[] args) {
+        int num = 1;
+
+        do {
+            System.out.print("  |");
+            for(int z=1; z<=9; z++) {
+                System.out.printf("%3d",z);
+            }
+            System.out.println();
+            System.out.println("---+---------------------------");
+
+            for(int i=1; i<=9; i++) {
+                System.out.printf(i + " |");
+                for (int j = 1; j <= 9; j++) {
+                    System.out.printf("%3d", i + j);
+                }
+                System.out.println();
+                num++;
+            }
+        }while(num>10);
+    }
+}
