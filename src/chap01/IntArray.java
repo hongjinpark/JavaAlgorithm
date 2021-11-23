055834dfc5037324a8817a0c16690fffe8aa2d24
ektkfk180 <81549627+ektkfk180@users.noreply.github.com>
2021-11-23 20:23:58 +0900

    배열의 특성 확인

---
+++ src/chap01/IntArray.java
@@ -0,0 +1,18 @@
+package chap01;
+
+public class IntArray {
+
+    public static void main(String[] args) {
+        
+        int[] a = new int[5]; // 배열의 선언
+
+        a[1] = 37; // a[1]에 37을 대입
+        a[2] = 51; // a[2]에 51을 대입
+        a[4] = a[1] * 2; // a[4]에  a[1] * 2, 74를 대입
+
+        for (int i = 0; i < a.length; i++) {
+
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+    }
+}
